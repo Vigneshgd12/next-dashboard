0fe717e83f8d65b4a3a28e8b952499c8cd289e1e
Vignesh <gdanandh@gmail.com>
2024-03-18 13:57:01 +1100

    Add tutorial changes

---
+++ app/ui/fonts.ts
@@ -0,0 +1,5 @@
+import { Inter, Lusitana, Yusei_Magic } from 'next/font/google';
+ 
+export const inter = Inter({ subsets: ['latin'] });
+export const lusitana = Lusitana({ weight: ['400', '700'],subsets: ['latin'] });   
+export const yuseiMagic = Yusei_Magic({ weight: ['400'],subsets: ['latin'] });   
